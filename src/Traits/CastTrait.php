0d1f3c42674733a7322bbf97e728ccaad7698d0f
Alejandro Mostajo <amostajo@gmail.com>
2018-06-18 13:31:10 -0600

    Extended array casting.
    Casting now successfully casts properties of type `object`|`array` and
    or object extended from `Model` class.

--- src/Traits/CastTrait.php
+++ src/Traits/CastTrait.php
@@ -8,13 +8,14 @@ namespace TenQuality\Data\Traits;
  * @author Cami M <info@10quality.com>
  * @copyright 10 Quality <info@10quality.com>
  * @package TenQuality\Data\Model
- * @version 1.0.0
+ * @version 1.0.1
  */
 trait CastTrait
 {
     /**
      * Returns model as array.
      * @since 1.0.0
+     * @since 1.0.1 Extended casting for object|array|Model values.
      *
      * @return array
      */
@@ -23,9 +24,8 @@ trait CastTrait
         $output = [];
         $value = null;
         foreach ($this->properties as $property) {
-            $value = $this->$property;
-            if ($value !== null)
-                $output[$property] = $value;
+            if ($this->$property !== null)
+                $output[$property] = $this->__getCleaned($this->$property);
         }
         return $output;
     }
@@ -48,5 +48,33 @@ trait CastTrait
     public function __toString()
     {
         return json_encode($this->__toArray());
+    }
+    /**
+     * Returns cleaned value for casting.
+     * @since 1.0.1
+     *
+     * @param mixed $value Value to clean.
+     *
+     * @return mixed
+     */
+    private function __getCleaned($value)
+    {
+        switch (gettype($value)) {
+            case 'object':
+                return method_exists($value, '__toArray')
+                    ? $value->__toArray()
+                    : (method_exists($value, 'toArray')
+                        ? $value->toArray()
+                        :(array)$value
+                    );
+            case 'array':
+                $output = [];
+                foreach ($value as $key => $data) {
+                    if ($data !== null)
+                        $output[$key] = $this->__getCleaned($data);
+                }
+                return $output;
+        }
+        return $value;
     }
 }
--- tests/cases/CastingTest.php
+++ tests/cases/CastingTest.php
@@ -6,7 +6,7 @@
  * @author Cami M <info@10quality.com>
  * @copyright 10 Quality <info@10quality.com>
  * @package TenQuality\Data\Model
- * @version 1.0.0
+ * @version 1.0.1
  */
 class CastingTest extends PHPUnit_Framework_TestCase
 {
@@ -47,5 +47,97 @@ class CastingTest extends PHPUnit_Framework_TestCase
         // Assert
         $this->assertInternalType('string', $json);
         $this->assertEquals('{"price":19.99,"displayPrice":"$19.99"}', $json);
+    }
+    /**
+     * String casting.
+     * @since 1.0.1
+     */
+    public function testWithObjectArrayCasting()
+    {
+        // Prepare
+        $model = new TestModel;
+        $model->code = 'Test';
+        $obj = new stdClass;
+        $obj->id = 7;
+        $obj->name = 'Bond';
+        $model->agent = $obj;
+        $model->properties = ['code','agent'];
+        // Execute
+        $array = $model->toArray();
+        // Assert
+        $this->assertInternalType('array', $array);
+        $this->assertArrayHasKey('code', $array);
+        $this->assertArrayHasKey('agent', $array);
+        $this->assertEquals('Test', $array['code']);
+        $this->assertInternalType('array', $array['agent']);
+        $this->assertArrayHasKey('id', $array['agent']);
+        $this->assertArrayHasKey('name', $array['agent']);
+        $this->assertEquals(7, $array['agent']['id']);
+        $this->assertEquals('Bond', $array['agent']['name']);
+    }
+    /**
+     * String casting.
+     * @since 1.0.1
+     */
+    public function testWithModelArrayCasting()
+    {
+        // Prepare
+        $model = new TestModel;
+        $model->code = 'Parent';
+        $sub = new TestModel;
+        $sub->code = 'Child';
+        $sub->price = 19.99;
+        $sub->properties = ['code','displayPrice'];
+        $model->sub = $sub;
+        $model->properties = ['code','sub'];
+        // Execute
+        $array = $model->toArray();
+        // Assert
+        $this->assertInternalType('array', $array);
+        $this->assertArrayHasKey('code', $array);
+        $this->assertArrayHasKey('sub', $array);
+        $this->assertEquals('Parent', $array['code']);
+        $this->assertInternalType('array', $array['sub']);
+        $this->assertArrayHasKey('code', $array['sub']);
+        $this->assertArrayHasKey('displayPrice', $array['sub']);
+        $this->assertEquals('Child', $array['sub']['code']);
+        $this->assertEquals('$19.99', $array['sub']['displayPrice']);
+    }
+    /**
+     * String casting.
+     * @since 1.0.1
+     */
+    public function testWithInnerArrayCasting()
+    {
+        // Prepare
+        $model = new TestModel;
+        $model->children = [7];
+        $child = new TestModel;
+        $child->code = 'Model';
+        $child->properties = ['code'];
+        $model->children[] = $child;
+        $child = new stdClass;
+        $child->id = 1;
+        $child->code = 'Object';
+        $model->children[] = $child;
+        $model->children[] = ['a','b','c'];
+        $model->properties = ['children'];
+        // Execute
+        $array = $model->toArray();
+        // Assert
+        $this->assertInternalType('array', $array);
+        $this->assertArrayHasKey('children', $array);
+        $this->assertInternalType('array', $array['children']);
+        $this->assertEquals(4, count($array['children']));
+        $this->assertEquals(7, $array['children'][0]);
+        $this->assertInternalType('array', $array['children'][1]);
+        $this->assertEquals('Model', $array['children'][1]['code']);
+        $this->assertInternalType('array', $array['children'][2]);
+        $this->assertEquals(1, $array['children'][2]['id']);
+        $this->assertEquals('Object', $array['children'][2]['code']);
+        $this->assertInternalType('array', $array['children'][3]);
+        $this->assertEquals('a', $array['children'][3][0]);
+        $this->assertEquals('b', $array['children'][3][1]);
+        $this->assertEquals('c', $array['children'][3][2]);
     }
 }
